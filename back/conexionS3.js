d9a1bf773ea7023889bcf2d513b0fbc7fa960c80
jcastlop <127107986+jcastlop@users.noreply.github.com>
2023-04-26 08:30:17 +0200

    actualizacion claves aws

--- back/conexionS3.js
+++ back/conexionS3.js
@@ -6,9 +6,9 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 const s3 = new S3Client({
   region: "us-east-1",
   credentials: {
-    accessKeyId: "ASIATQ5KEFU7LQX53O4C",
-    secretAccessKey: "G+6iaKYlENjFxS1Y0x8IL52FmkbXVc6nlm8wVf40",
-    sessionToken: "FwoGZXIvYXdzEBkaDKyOm0uJBHYlnnAZeyK+AYhHuq0cdr1BHxrG1dpeFUm1bz5zN/GdcFC/3dkSiP8c948jHhgW1xSYqs9AL4PMapzIhK8NGeaD/sssR22eTnSPbBl0MUh8vD7KTI09+1pNN4815UFpr6mzBSyrMl/XyZUORzGjClHeWWXJz0JgKHCHlrSvaKeNY6fiFnIktoouMXNhXb/7tcu4yeD6R6Viz0ueX0O4fMmRpH/o9s+0eYQcm9SRZ+caqvkcy0VFeKXPTKYynlU/u9fSwmEhlKso5IieogYyLQC8VYEs4nFgtCSrtcJx0Ocx0dqh8ziitPerphvr0Wrq8t1/N5dESefDBmyZXw=="
+    accessKeyId: "ASIATQ5KEFU7MRSNWY3S",
+    secretAccessKey: "afKpcUbHaInGU/X0qjPKC+vIeBaiLWobGoYSRBpp",
+    sessionToken: "FwoGZXIvYXdzEDAaDO790rCSJ7kx5VQdhSK+AZkz49bC1mjzlt73QxJ4LUnTy2keS6mTE3HXIfOcJneN4w/mnyJQuqY/bLF3FU4oLF008VF11oEJ6K3LuLWIGS3wXcq3BfFTBX5hHQani0LxMs3Ic09SN0Vmb5RWB/3HCeceQorpq2ZLIdkwqVcUcb0SgykXYrtrBfKSDIJygLyms6hBlHGuq367ZuhjcCNiSsl+WiBnDX9gkGWeRQz/bjeQajhto02vq7HRDg5WWr+VYxfjcHqOQVqTiRbQv2kolIijogYyLf+f7uXZOyEuVrhBUCbnISuCA05Qhnhg6H3dTiaVYhc/tndkpl5tqyLR2PAUHQ=="
   }
 });
 
