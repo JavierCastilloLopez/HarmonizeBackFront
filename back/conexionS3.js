a271ea37a6678210efbd7d136d6eaa4475b29813
JavierCastilloLopez <106767077+JavierCastilloLopez@users.noreply.github.com>
2023-06-05 09:47:27 +0200

    add

--- back/conexionS3.js
+++ back/conexionS3.js
@@ -6,15 +6,15 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 const s3 = new S3Client({
   region: "us-east-1",
   credentials: {
-    accessKeyId: "ASIATQ5KEFU7OZKRIZVS",
-    secretAccessKey: "f6NqHnvZojCzzw/2Men52rEVVu09riZ7dIvXndm9",
-    sessionToken: "FwoGZXIvYXdzELv//////////wEaDFaHdcPPpGsNUwnMTiK+AbjVu4MoRaGFZl3oe9jGtOaLzFYRb1fvztVR60EZYyaEBbd9y4uBGacMp2k/chh6oyz1E5h4clHmN8oS8vf36QyJLuu8KdCgLyN60X1oFTkfOlx2PqwhBCu1q/YWIEdHDBeEhNeu1fE9G+D/3oFaY1JN8eXlleni+hfsWiEy/LvbqyqOEH5mq6ZUbi7hpnm48dslHQTKatckYNnXNyTYOWqRnHQIJ7WwhcI0VUsZGdLkfSbpwswb2Sxpw7DYu4go/ZmyowYyLWD3vSlwf574bROjIKum9IBAO4FbJUVXsSJ+s7Mro+YQ1Tytcchxcwf17ItwBg=="
+    accessKeyId: "ASIATQ5KEFU7NROGABHM",
+    secretAccessKey: "1OzJMFOcd9QRKAfJQ3Ju1VqzstM03mT7bWQ+LAip",
+    sessionToken: "FwoGZXIvYXdzEPH//////////wEaDOmMOUtOpODJ/fDCWCK+AaVdyzp+Kg3UAqVjPzupYgtca9Xn8MAmAiOf9BdbEA2BiNkvbPbQO085l2A3dVYg/UJ0AFRmAKNj6bkkoGDzjZtSD3Iyj/ln5zmciLIx9a2Lpr9ZWT7L4gr4vopf+R5qcPBAMBS2yd6gaOmODLz+xieB3AcWQUiuDmxSRBDQxjb9e1kmA20z7Y3ro1S62E55qXMIoXY7SkWLxW9DtKKELyI2nhxNqBxkzXjzikI+tLCPyd2rtLNfGFyKuzYNwjgo3qT2owYyLcGQJgMVsXU89jzO6fUT60h2WUbZ1MQVaFbIG39IyR3N8zT6w6UE/jdLyCzBMQ=="
   }
 });
 
 
 export async function subirArchivoAS3(archivo, bucket, nombreArchivo, acl = 'private', contentType = '') {
-    
+
     const params = {
       Bucket: bucket,
       Key: nombreArchivo,
@@ -23,7 +23,7 @@ export async function subirArchivoAS3(archivo, bucket, nombreArchivo, acl = 'pri
       ContentType: contentType,
     };
     const command = new PutObjectCommand(params);
-  
+
     try {
       const response = await s3.send(command);
       console.log('Archivo subido con éxito a S3: ', response.Location);
@@ -33,4 +33,4 @@ export async function subirArchivoAS3(archivo, bucket, nombreArchivo, acl = 'pri
       throw error;
     }
   }
-  
+
